bbb849a9ff12ecda9fc24ff158b70c4b5ac76ea7
Prithviraj Maity <prithviraj.maity@superprocure.com>
2024-04-13 14:05:39 +0530

    .

--- server.js
+++ server.js
@@ -9,13 +9,7 @@ const port = 3001;
 app.use(bodyParser.json());
 app.use('/api', appRouter);
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
- })
-
- app.use(cors());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
