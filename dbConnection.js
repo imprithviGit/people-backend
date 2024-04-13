5c879ff73e4f66ff1b8a5e20368d241cb571d621
Prithviraj Maity <prithviraj.maity@superprocure.com>
2024-04-14 00:33:02 +0530

    connection revert

--- dbConnection.js
+++ dbConnection.js
@@ -1,35 +1,15 @@
 const mysql = require('mysql');
 
-let dbConnection;
+const dbConnection = mysql.createConnection({
+    host: 'sql6.freemysqlhosting.net',
+    user: 'sql6698319',
+    password: 'aNKmnUVDuW',
+    database: 'sql6698319'
+});
 
-function handleDisconnect() {
-    dbConnection = mysql.createConnection({
-        host: 'sql6.freemysqlhosting.net',
-        user: 'sql6698319',
-        password: 'aNKmnUVDuW',
-        database: 'sql6698319',
-        reconnect: true 
-    });
-
-    dbConnection.connect((err) => {
-        if (err) {
-            console.error('Error connecting to MySQL:', err);
-            setTimeout(handleDisconnect, 2000); 
-        } else {
-            console.log('Connected to MySQL database!');
-        }
-    });
-
-    dbConnection.on('error', (err) => {
-        console.error('MySQL connection error:', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect(); 
-        } else {
-            throw err;
-        }
-    });
-}
-
-handleDisconnect(); 
+dbConnection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL database!');
+});
 
 module.exports = dbConnection;
