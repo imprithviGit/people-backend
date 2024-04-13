57868a0213791d49db9f1eab392527233bfd847d
Prithviraj Maity <prithviraj.maity@superprocure.com>
2024-04-13 21:13:43 +0530

    connection fixes

--- dbConnection.js
+++ dbConnection.js
@@ -1,15 +1,35 @@
 const mysql = require('mysql');
 
-const dbConnection = mysql.createConnection({
-    host: 'sql6.freemysqlhosting.net',
-    user: 'sql6698319',
-    password: 'aNKmnUVDuW',
-    database: 'sql6698319'
-});
+let dbConnection;
 
-dbConnection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL database!');
-});
+function handleDisconnect() {
+    dbConnection = mysql.createConnection({
+        host: 'sql6.freemysqlhosting.net',
+        user: 'sql6698319',
+        password: 'aNKmnUVDuW',
+        database: 'sql6698319',
+        reconnect: true 
+    });
+
+    dbConnection.connect((err) => {
+        if (err) {
+            console.error('Error connecting to MySQL:', err);
+            setTimeout(handleDisconnect, 2000); 
+        } else {
+            console.log('Connected to MySQL database!');
+        }
+    });
+
+    dbConnection.on('error', (err) => {
+        console.error('MySQL connection error:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect(); 
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect(); 
 
 module.exports = dbConnection;
