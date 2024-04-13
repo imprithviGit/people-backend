c941ed2e3cf33ad013819915df535c6d93bba5fb
Prithviraj Maity <prithviraj.maity@superprocure.com>
2024-04-13 13:38:30 +0530

    cors change

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const appRouter = require('./router');
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
 app.use('/api', appRouter);
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
